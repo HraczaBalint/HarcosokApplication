cd51fa5f0cec9d85435560fac064119d5d8cb62a
HraczaBalint <hraczabalintmail@gmail.com>
2020-12-31 18:51:11 +0100

    Add project files.

---
+++ HarcosokApplication/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace HarcosokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.harcosNeveTextBox = new System.Windows.Forms.TextBox();
+            this.letrehozasButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.kepessegNeveTextBox = new System.Windows.Forms.TextBox();
+            this.HasznaloComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.leirasTextBox = new System.Windows.Forms.TextBox();
+            this.hozzaadasButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.harcosokListBox = new System.Windows.Forms.ListBox();
+            this.kepessegekListBox = new System.Windows.Forms.ListBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.kepessegLeirasaTextBox = new System.Windows.Forms.TextBox();
+            this.modositasButton = new System.Windows.Forms.Button();
+            this.torlesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Harcosok létrehozása";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Név:";
+            // 
+            // harcosNeveTextBox
+            // 
+            this.harcosNeveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.harcosNeveTextBox.Location = new System.Drawing.Point(72, 57);
+            this.harcosNeveTextBox.Name = "harcosNeveTextBox";
+            this.harcosNeveTextBox.Size = new System.Drawing.Size(200, 30);
+            this.harcosNeveTextBox.TabIndex = 2;
+            // 
+            // letrehozasButton
+            // 
+            this.letrehozasButton.AutoSize = true;
+            this.letrehozasButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.letrehozasButton.Location = new System.Drawing.Point(362, 55);
+            this.letrehozasButton.Name = "letrehozasButton";
+            this.letrehozasButton.Size = new System.Drawing.Size(119, 35);
+            this.letrehozasButton.TabIndex = 3;
+            this.letrehozasButton.Text = "Létrehozás";
+            this.letrehozasButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(0, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(855, 3);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(13, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Képesség hozzáadása";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(13, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Használnó:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(12, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Név:";
+            // 
+            // kepessegNeveTextBox
+            // 
+            this.kepessegNeveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kepessegNeveTextBox.Location = new System.Drawing.Point(130, 251);
+            this.kepessegNeveTextBox.Name = "kepessegNeveTextBox";
+            this.kepessegNeveTextBox.Size = new System.Drawing.Size(200, 30);
+            this.kepessegNeveTextBox.TabIndex = 8;
+            // 
+            // HasznaloComboBox
+            // 
+            this.HasznaloComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HasznaloComboBox.FormattingEnabled = true;
+            this.HasznaloComboBox.Location = new System.Drawing.Point(130, 188);
+            this.HasznaloComboBox.Name = "HasznaloComboBox";
+            this.HasznaloComboBox.Size = new System.Drawing.Size(200, 33);
+            this.HasznaloComboBox.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(388, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 25);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Leírás:";
+            // 
+            // leirasTextBox
+            // 
+            this.leirasTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.leirasTextBox.Location = new System.Drawing.Point(465, 188);
+            this.leirasTextBox.Multiline = true;
+            this.leirasTextBox.Name = "leirasTextBox";
+            this.leirasTextBox.Size = new System.Drawing.Size(330, 93);
+            this.leirasTextBox.TabIndex = 11;
+            // 
+            // hozzaadasButton
+            // 
+            this.hozzaadasButton.AutoSize = true;
+            this.hozzaadasButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hozzaadasButton.Location = new System.Drawing.Point(12, 303);
+            this.hozzaadasButton.Name = "hozzaadasButton";
+            this.hozzaadasButton.Size = new System.Drawing.Size(121, 35);
+            this.hozzaadasButton.TabIndex = 12;
+            this.hozzaadasButton.Text = "Hozzáadás";
+            this.hozzaadasButton.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Location = new System.Drawing.Point(0, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(855, 3);
+            this.label8.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(12, 396);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Harcosok:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(254, 396);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(122, 25);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Képességei:";
+            // 
+            // harcosokListBox
+            // 
+            this.harcosokListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.harcosokListBox.FormattingEnabled = true;
+            this.harcosokListBox.ItemHeight = 25;
+            this.harcosokListBox.Location = new System.Drawing.Point(17, 442);
+            this.harcosokListBox.Name = "harcosokListBox";
+            this.harcosokListBox.Size = new System.Drawing.Size(200, 279);
+            this.harcosokListBox.TabIndex = 16;
+            // 
+            // kepessegekListBox
+            // 
+            this.kepessegekListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kepessegekListBox.FormattingEnabled = true;
+            this.kepessegekListBox.ItemHeight = 25;
+            this.kepessegekListBox.Location = new System.Drawing.Point(259, 442);
+            this.kepessegekListBox.Name = "kepessegekListBox";
+            this.kepessegekListBox.Size = new System.Drawing.Size(200, 279);
+            this.kepessegekListBox.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(499, 396);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(169, 25);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Képesség leírása:";
+            // 
+            // kepessegLeirasaTextBox
+            // 
+            this.kepessegLeirasaTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kepessegLeirasaTextBox.Location = new System.Drawing.Point(504, 442);
+            this.kepessegLeirasaTextBox.Multiline = true;
+            this.kepessegLeirasaTextBox.Name = "kepessegLeirasaTextBox";
+            this.kepessegLeirasaTextBox.Size = new System.Drawing.Size(330, 93);
+            this.kepessegLeirasaTextBox.TabIndex = 19;
+            // 
+            // modositasButton
+            // 
+            this.modositasButton.AutoSize = true;
+            this.modositasButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.modositasButton.Location = new System.Drawing.Point(715, 561);
+            this.modositasButton.Name = "modositasButton";
+            this.modositasButton.Size = new System.Drawing.Size(119, 35);
+            this.modositasButton.TabIndex = 20;
+            this.modositasButton.Text = "Módosítás";
+            this.modositasButton.UseVisualStyleBackColor = true;
+            // 
+            // torlesButton
+            // 
+            this.torlesButton.AutoSize = true;
+            this.torlesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.torlesButton.Location = new System.Drawing.Point(259, 751);
+            this.torlesButton.Name = "torlesButton";
+            this.torlesButton.Size = new System.Drawing.Size(119, 35);
+            this.torlesButton.TabIndex = 21;
+            this.torlesButton.Text = "Törlés";
+            this.torlesButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 808);
+            this.Controls.Add(this.torlesButton);
+            this.Controls.Add(this.modositasButton);
+            this.Controls.Add(this.kepessegLeirasaTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.kepessegekListBox);
+            this.Controls.Add(this.harcosokListBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.hozzaadasButton);
+            this.Controls.Add(this.leirasTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.HasznaloComboBox);
+            this.Controls.Add(this.kepessegNeveTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.letrehozasButton);
+            this.Controls.Add(this.harcosNeveTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Harcosok";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox harcosNeveTextBox;
+        private System.Windows.Forms.Button letrehozasButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox kepessegNeveTextBox;
+        private System.Windows.Forms.ComboBox HasznaloComboBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox leirasTextBox;
+        private System.Windows.Forms.Button hozzaadasButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox harcosokListBox;
+        private System.Windows.Forms.ListBox kepessegekListBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox kepessegLeirasaTextBox;
+        private System.Windows.Forms.Button modositasButton;
+        private System.Windows.Forms.Button torlesButton;
+    }
+}
+
